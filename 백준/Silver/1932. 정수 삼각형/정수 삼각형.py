f21675596f3ca10985f7121c2f5589fc3582f584
Taeyoung Kim <71509981+taeyk1@users.noreply.github.com>
2024-09-02 16:47:09 +0900

    [Silver I] Title: 정수 삼각형, Time: 112 ms, Memory: 111576 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -6,6 +6,6 @@ dp = [list(map(int, input().split())) for i in range(n)]
 for i in range(n-1, 0, -1):
     temp = []
     for j in range(0, i):
-            dp[i-1][j] = max([dp[i-1][j]+dp[i][j], dp[i-1][j]+dp[i][j+1]])
+        dp[i-1][j] = max([dp[i-1][j]+dp[i][j], dp[i-1][j]+dp[i][j+1]]) # bottom-up dp
 print(max(dp[0]))
     
