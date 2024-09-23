54e9b4c968901c065f74be8fade8ead09ade40c8
Taeyoung Kim <71509981+taeyk1@users.noreply.github.com>
2024-09-23 10:54:30 +0900

    [level 2] Title: 땅따먹기, Time: 207.41 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,12 +1,7 @@
 def solution(land):
     for row in range(1, len(land)):
         for current_col in range(4):
-            before_value = land[row][current_col]
-            for roatate_col in range(4):
-                if current_col != roatate_col:
-                    land[row][current_col] = max(land[row][current_col],before_value+land[row-1][roatate_col])
-            
-    answer = max(land[-1])
-    return answer
+            land[row][current_col] = land[row][current_col] + max(land[row-1][:current_col] + land[row-1][current_col+1:])
+    return max(land[-1])
 
         
