f5b2170eaab2d2e7b2cd678db09894f2ae2c7e14
Taeyoung Kim <71509981+taeyk1@users.noreply.github.com>
2024-09-24 21:10:17 +0900

    [level 1] Title: 문자열 나누기, Time: 4.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -0,0 +1,18 @@
+def solution(s):
+    x_count, not_x_count, answer = 0, 0, 0
+    x = s[0]
+    for i in range(len(s)):
+        if s[i] == x:
+            x_count+=1
+        else: 
+            not_x_count+=1
+        if x_count == not_x_count:
+            answer+=1
+            if i != len(s)-1:
+                x = s[i+1]
+                x_count, not_x_count = 0, 0
+            else:
+                break
+        elif x_count != not_x_count and i == len(s)-1:
+            answer+=1
+    return answer
